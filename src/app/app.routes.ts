7b7850486438689969c27bca03dbdb0310bffca7
nikitha-kumari <nikitha0476@gmail.com>
2026-03-13 21:57:03 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Splash } from './components/splash/splash';
+import { Login} from './components/login/login';
+import { Signup } from './components/signup/signup';
+
+export const routes: Routes = [
+  { path: '', component: Splash },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup}
+];
