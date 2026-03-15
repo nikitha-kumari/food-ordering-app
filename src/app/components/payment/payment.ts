f7a4eb8efe9f6a680f9cd9cbe22e2c6704819b2e
nikitha-kumari <nikitha0476@gmail.com>
2026-03-15 19:08:31 +0530

    Added project files

--- src/app/components/payment/payment.ts
+++ src/app/components/payment/payment.ts
@@ -1,28 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { CartService } from '../../services/cart';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './payment.html',
   styleUrl: './payment.css'
 })
-export class Payment{
+export class Payment {
 
-  constructor(private cartService: CartService,
-              private router: Router) {}
+  paymentMethod = '';
 
-  placeOrder(){
+  constructor(
+    private cartService: CartService,
+    private router: Router
+  ) {}
+
+  
+ placeOrder(){
+
+  if(!this.paymentMethod){
+    alert("Select payment method");
+    return;
+  }
+
+  if(this.paymentMethod === 'upi' && !this.upiVerified){
+    alert("Please verify UPI ID");
+    return;
+  }
 
   const orders = JSON.parse(localStorage.getItem('orders') || '[]');
 
   orders.push({
     items: this.cartService.getItems(),
     total: this.cartService.getTotal(),
-    date: new Date()
+    date: new Date(),
+    payment: this.paymentMethod
   });
 
   localStorage.setItem('orders', JSON.stringify(orders));
@@ -30,6 +47,21 @@ export class Payment{
   this.cartService.clearCart();
 
   this.router.navigate(['/order-success']);
+
+}
+upiId = '';
+upiVerified = false;
+
+verifyUPI(){
+
+  const upiRegex =  /^[\w.-]+@[\w]+$/;
+
+  if(upiRegex.test(this.upiId)){
+    this.upiVerified = true;
+  }
+  else{
+    alert("Invalid UPI ID");
+  }
 
 }
 
